f08175b162aa3cff4550f366e13d2e3c27f4266d
Vaibhav Srivastav <vaibhavs10@gmail.com>
2021-12-07 13:53:31 +0100

    Removing Nystromformer forward pass code

--- Layers/Attention.py
+++ Layers/Attention.py
@@ -162,61 +162,6 @@ class NystromAttention(nn.Module):
 
         return out
 
-# transformer
-
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        x = self.norm(x)
-        return self.fn(x, **kwargs)
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, mult = 4, dropout = 0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, dim * mult),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(dim * mult, dim)
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-class Nystromformer(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        depth,
-        dim_head = 64,
-        heads = 8,
-        num_landmarks = 256,
-        pinv_iterations = 6,
-        attn_values_residual = True,
-        attn_values_residual_conv_kernel = 33,
-        attn_dropout = 0.,
-        ff_dropout = 0.   
-    ):
-        super().__init__()
-
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, NystromAttention(dim = dim, dim_head = dim_head, heads = heads, num_landmarks = num_landmarks, pinv_iterations = pinv_iterations, residual = attn_values_residual, residual_conv_kernel = attn_values_residual_conv_kernel, dropout = attn_dropout)),
-                PreNorm(dim, FeedForward(dim = dim, dropout = ff_dropout))
-            ]))
-
-    def forward(self, x, mask = None):
-        for attn, ff in self.layers:
-            x = attn(x, mask = mask) + x
-            x = ff(x) + x
-        return x
-
 
 class MultiHeadedAttention(nn.Module):
     """
