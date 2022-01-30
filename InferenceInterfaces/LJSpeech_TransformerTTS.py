1e4c6bd400ac50550ccd9be42006b3a7e17f63dd
Vaibhav Srivastav <st175539>
2022-01-30 21:37:04 +0100

    couple more changes to support hifigan

--- InferenceInterfaces/LJSpeech_TransformerTTS.py
+++ InferenceInterfaces/LJSpeech_TransformerTTS.py
@@ -19,10 +19,10 @@ class LJSpeech_TransformerTTSInference(torch.nn.Module):
         self.text2phone = TextFrontend(language="en", use_word_boundaries=False,
                                        use_explicit_eos=False, inference=True)
         try:
-            self.phone2mel = Transformer(path_to_weights=os.path.join("Models", "TransformerTTS_LJSpeech", "best.pt"),
+            self.phone2mel = Transformer(path_to_weights=os.path.join("Models", "best_models", "best_reformer_3xlong.pt"),
                                          idim=166, odim=80, spk_embed_dim=None, reduction_factor=1).to(torch.device(device))
         except RuntimeError:
-            self.phone2mel = Transformer(path_to_weights=os.path.join("Models", "TransformerTTS_LJSpeech", "best.pt"),
+            self.phone2mel = Transformer(path_to_weights=os.path.join("Models", "best_models", "best_reformer_3xlong.pt"),
                                          idim=166, odim=80, spk_embed_dim=None, reduction_factor=1, legacy_model=True).to(torch.device(device))
         # self.mel2wav = MelGANGenerator(path_to_weights=os.path.join("Models", "MelGAN_LJSpeech", "best.pt")).to(torch.device(device))
         self.mel2wav = HiFiGANGenerator(path_to_weights=os.path.join("Models", "hifigan_best.pt")).to(torch.device(device))
