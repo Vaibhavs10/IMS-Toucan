b6f89d553388beaa086458c96a84ba061d957fa2
vaibhavs10 <vaibhavs10@gmail.com>
2023-04-17 17:16:13 +0200

    up

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -9,6 +9,7 @@ from TrainingInterfaces.TrainingPipelines.PortaSpeech_MetaCheckpoint import run 
 from TrainingInterfaces.TrainingPipelines.finetuning_example import run as fine_tuning_example
 from TrainingInterfaces.TrainingPipelines.PS_ft_lj_no_postnet import run as ps_ft_lj_no_postnet
 from TrainingInterfaces.TrainingPipelines.PS_ft_lj_no_postnet_scratch import run as ps_ft_lj_no_postnet_scratch
+from TrainingInterfaces.TrainingPipelines.PS_ft_lj_ddpm_postnet import run as ps_ft_lj_ddpm_postnet_scratch
 from TrainingInterfaces.TrainingPipelines.pretrain_aligner import run as aligner
 
 pipeline_dict = {
@@ -20,7 +21,8 @@ pipeline_dict = {
     "fs_it"    : fs_integration_test,
     "ps_it"    : ps_integration_test,
     "ps_ft_lj" : ps_ft_lj_no_postnet,
-    "ps_ft_lj_scratch" : ps_ft_lj_no_postnet_scratch,
+    "ps_ft_lj_scratch" : ps_ft_lj_ddpm_postnet_scratch,
+
 }
 
 if __name__ == '__main__':
