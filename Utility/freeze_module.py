458034055272a66cee96938be8fce3b2ed7455ea
vaibhavs10 <vaibhavs10@gmail.com>
2023-05-05 17:54:14 +0200

    up

--- Utility/freeze_module.py
+++ Utility/freeze_module.py
@@ -6,10 +6,12 @@ def freeze_postnet(net):
 def freeze_all_except_postnet(net):
     net.encoder.requires_grad_(False)
     net.duration_predictor.requires_grad_(False)
+    net.pitch_predictor.requires_grad_(False)    
     net.pitch_embed.requires_grad_(False)
     net.energy_predictor.requires_grad_(False)
     net.energy_embed.requires_grad_(False)
     net.length_regulator.requires_grad_(False)
+    net.decoder.requires_grad_(False)
     net.feat_out.requires_grad_(False)
     net.pitch_bottleneck.requires_grad_(False)
     net.energy_bottleneck.requires_grad_(False)
