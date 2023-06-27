642c5cc2ee04e20dd0b6d7ea521b430f6ad56905
vaibhavs10 <vaibhavs10@gmail.com>
2023-06-27 20:51:24 +0200

    up

--- run_utterance_cloner.py
+++ run_utterance_cloner.py
@@ -1,16 +1,36 @@
+import os
 import torch
 
 from InferenceInterfaces.UtteranceCloner import UtteranceCloner
 
-if __name__ == '__main__':
-    # uc = UtteranceCloner(model_id="Models/PortaSpeech_LJSpeech_Flow_0_001/checkpoint_100170.pt", device="cuda" if torch.cuda.is_available() else "cpu")
-    uc = UtteranceCloner(model_id="Models/PortaSpeech_LJSpeech_Flow_0_001/checkpoint_100170.pt", device="cpu")
 
-    uc.clone_utterance(path_to_reference_audio="audios/lj_50/wav/LJ050-0001.wav",
-                       reference_transcription="For more information, or to volunteer, please visit librivox dot org. Report of the President's Commission on the Assassination of President Kennedy.",
-                       filename_of_result="audios/test_cloned_spk_id_t.wav",
-                       clone_speaker_identity=False,
-                       lang="en")
+def create_mos_survey_samples(model_id):
+    uc = UtteranceCloner(model_id=model_id, device="cuda" if torch.cuda.is_available() else "cpu")
+    
+    output_dir = f"audios/MOS_Survey_{model_id}"
+    
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    
+    for file_num in ["112", "181", "174", "126", "165", "072"]:
+        
+        base_file_path = f"/mount/resources/speech/corpora/LJSpeech/16kHz/txt/LJ050-0{file_num}.txt"
+
+        with open(base_file_path, 'r', encoding='utf8') as base_tf:
+            transcript = base_tf.read()
+        
+        base_wav_path = f"/mount/resources/speech/corpora/LJSpeech/16kHz/wav/LJ050-0{file_num}.wav"
+        
+        uc.clone_utterance(
+            path_to_reference_audio=base_wav_path,
+            reference_transcription=transcript,
+            filename_of_result=output_dir+f"/{file_num}.wav",
+            clone_speaker_identity=False,
+            lang="en")
+
+
+if __name__ == '__main__':
+    create_mos_survey_samples(model_id="LJSpeech_FrozenFlow_0_002_15k")
 
     # uc.biblical_accurate_angel_mode(path_to_reference_audio="audios/test.wav",
     #                                 reference_transcription="Hello world, this is a test.",
--- run_weight_averaging.py
+++ run_weight_averaging.py
@@ -155,5 +155,5 @@ def show_all_models_params():
 
 if __name__ == '__main__':
 #    make_best_in_all()
-    model_dir = "/mount/arbeitsdaten56/projekte/synthesis/srivasvv/thesis-playground/tts-postnet-eval/IMS-Toucan/Models/PortaSpeech_LJSpeech_FrozenFlow_0_001"
+    model_dir = "/mount/arbeitsdaten56/projekte/synthesis/srivasvv/thesis-playground/tts-postnet-eval/IMS-Toucan/Models/PortaSpeech_LJSpeech_FrozenFlow_0_002_15k"
     avg_and_save_tts_model(model_dir)
